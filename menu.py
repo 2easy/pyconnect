f861fd87c169f2bb489d7259888ebb438635d30e
Piotr Olchawa <piotrekolchawa@gmail.com>
2011-12-29 19:33:31 +0100

    menu label added

--- menu.py
+++ menu.py
@@ -1,15 +1,17 @@
 import curses
 
 class Menu():
+    # menu paddings
+    h_pad = 2 + 1
+    v_pad = 1 + 1
     def __init__(self, max_x=0,max_y=0, options = []):
         curses.initscr()
         #determining height and width
         self.width = 0
         for o in options:
             if len(o) > self.width: self.width = len(o)
-        # +2 for up/bottom, left/right borders
-        self.width  += 2
-        self.height = len(options) + 2
+        self.width  += Menu.h_pad*2
+        self.height = len(options) + Menu.v_pad*2
         # get center coordinates
         x = (max_x - self.width)/2
         y = (max_y - self.height)/2
@@ -19,25 +21,23 @@ class Menu():
         # fill it with the given options
         self.curr_opt = 0
         self.last_opt = len(options)-1
-        i = 1
-        for o in options:
-            if (i-1 == self.curr_opt):
-                self.win.addstr(i,1,o,curses.A_STANDOUT)
-            else:
-                self.win.addstr(i,1,o)
-            i+=1
         self.options  = options
+        self.display()
     def next_opt(self):
         self.curr_opt = (self.curr_opt + 1) % len(self.options)
     def prev_opt(self):
         if self.curr_opt > 0: self.curr_opt -= 1
         else: self.curr_opt = self.last_opt
     def display(self):
-        i = 1
+        # create nice menu label
+        x = (self.width-len(" MENU "))/2
+        self.win.addstr(0,x," MENU ")
+        # fill menu with supplied options
+        i = 0
         for o in self.options:
-            if (i-1 == self.curr_opt):
-                self.win.addstr(i,1,o,curses.A_STANDOUT)
+            if (i == self.curr_opt):
+                self.win.addstr(Menu.v_pad+i,Menu.h_pad,o,curses.A_STANDOUT)
             else:
-                self.win.addstr(i,1,o)
+                self.win.addstr(Menu.v_pad+i,Menu.h_pad,o)
             i+=1
         self.win.refresh()
--- ui.py
+++ ui.py
@@ -6,16 +6,21 @@ curses.KEY_ENTER = 10
 from menu import Menu
 
 try:
+    # Init curses
     scr = curses.initscr()
+    scr.keypad(1)
+
     curses.cbreak()
     curses.noecho()
+    curses.curs_set(0) #turn off the cursor
 
-    scr.keypad(1)
     max_y,max_x = scr.getmaxyx()
 
     #scr.addstr(0,0,str(max_x)+" "+str(max_y))
-
-    m = Menu(max_x,max_y,["1. Create new account", "2. Exit"])
+    m = Menu(max_x,max_y,["1. Login",
+                          "2. Create account (locally)",
+                          "3. Create account (on server)",
+                          "4. Exit"])
     scr.refresh()
     m.display()
     key = None
@@ -24,8 +29,8 @@ try:
         #scr.addstr(1,0,str(curses.KEY_UP))
         #scr.addstr(2,0,str(key))
 
-        if key == curses.KEY_UP: m.next_opt()
-        elif key == curses.KEY_DOWN: m.prev_opt()
+        if key == curses.KEY_UP: m.prev_opt()
+        elif key == curses.KEY_DOWN: m.next_opt()
         elif key == curses.KEY_ENTER: break
         m.display()
 
